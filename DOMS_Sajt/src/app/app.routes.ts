a73e1ef426c54712bae1f67b483d6613f6c734ad
srebronikola1 <srebronikola1@gmail.com>
2025-05-28 00:23:19 +0200

    --pocetna

--- DOMS_Sajt/src/app/app.routes.ts
+++ DOMS_Sajt/src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router';
-import { EnterijeriComponent } from './enterijeri/enterijeri.component';
-import { ModeliComponent } from './modeli/modeli.component';
 import { KontaktComponent } from './kontakt/kontakt.component';
-import { NamestajPoMeriComponent } from './namestaj-po-meri/namestaj-po-meri.component';
-import { ONamaComponent } from './o-nama/o-nama.component';
 import { AppComponent } from './app.component';
 import { PocetnaComponent } from './pocetna/pocetna.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { UslugeComponent } from './usluge/usluge.component';
 
 export const routes: Routes = [
-  {path: "enterijeri",component:EnterijeriComponent},
-  {path: "modeli" , component:ModeliComponent},
+  {path: "portfolio",component:PortfolioComponent},
+  {path:"usluge",component:UslugeComponent},
   {path: "kontakt",component:KontaktComponent},
-  {path: "namestaj_po_meri",component:NamestajPoMeriComponent},
-  {path: "o_nama",component:ONamaComponent},
   {path: "",component:PocetnaComponent}
 ];
