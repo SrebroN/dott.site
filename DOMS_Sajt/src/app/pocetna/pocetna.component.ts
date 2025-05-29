b6b63eea3b9f2806bf065d4906af44cea4582a77
srebronikola1 <srebronikola1@gmail.com>
2025-05-29 02:40:50 +0200

    --portfolio

--- DOMS_Sajt/src/app/pocetna/pocetna.component.ts
+++ DOMS_Sajt/src/app/pocetna/pocetna.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pocetna',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
   templateUrl: './pocetna.component.html',
   styleUrl: './pocetna.component.css'
 })
-export class PocetnaComponent {
+export class PocetnaComponent implements OnInit, OnDestroy {
+  constructor (private router:Router){}
+  images: string[] = [
+    "assets/slike/slideshow_dog_1.webp",
+    "assets/slike/slideshow_dog_2.avif",
+    "assets/slike/slideshow_dog_3.avif",
+    "assets/slike/slideshow_dog_4.jpg",
+  ]
+  currentIndex = 0;
+  intervalId: any;
+  ngOnInit(): void {
+    this.startAutoSlide();
+    this.router.events.subscribe((event)=>{
+      if(!(event instanceof NavigationEnd)){
+        return;
+      }
+      window.scrollTo(0,0);
+    })
+  }
 
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+  prevSlide(){
+    this.currentIndex=(this.currentIndex-1 +this.images.length)%this.images.length;
+  }
 }
--- DOMS_Sajt/src/app/portfolio/portfolio.component.ts
+++ DOMS_Sajt/src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
-export class PortfolioComponent {
-
+export class PortfolioComponent implements OnInit{
+  constructor (private router:Router){}
+  ngOnInit(){
+    this.router.events.subscribe((event)=>{
+      if(!(event instanceof NavigationEnd)){
+        return;
+      }
+      window.scrollTo(0,0);
+    })
+  }
 }
