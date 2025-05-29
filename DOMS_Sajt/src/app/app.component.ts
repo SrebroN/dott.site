478e6c7a93db77461e700a07f7b72ea5670f97d0
srebronikola1 <srebronikola1@gmail.com>
2025-05-29 23:16:28 +0200

    buttontotop

--- DOMS_Sajt/src/app/app.component.ts
+++ DOMS_Sajt/src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'app';
+  showTopButton: boolean = false;
   constructor(private scroller: ViewportScroller, private router: Router) { }
 
   ngOnInit() {
@@ -20,4 +22,13 @@ export class AppComponent implements OnInit {
     this.scroller.scrollToAnchor("toKontakt");
 
   }
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.showTopButton = window.scrollY > innerHeight;
+  }
+  scrollToTop() {
+    window.scrollTo({ top: 0 });
+  }
+
 }
+
