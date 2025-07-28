3c8855c40ef1c0a7df60dbb56bb2b6151abb039d
Nikola Srebro <srebronikola1@gmail.com>
2025-07-28 18:52:29 +0200

    Copy main to gh-pages, live mobile version 1.0

---
+++ DOMS_Sajt/src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { ViewportScroller } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
+
+import { MatIconModule } from '@angular/material/icon';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, MatIconModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  showTopButton: boolean = false;
+  constructor(private scroller: ViewportScroller, private router: Router) { }
+
+  ngOnInit() {
+    this.router.navigate(["/"]);
+  }
+  scrollToKontakt() {
+    this.scroller.scrollToAnchor("toKontakt");
+  }
+  scrollLogo() {
+    this.router.navigate(["/"]);
+    this.scrollToTop();
+  }
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.showTopButton = window.scrollY > innerHeight;
+  }
+  scrollToTop() {
+    window.scrollTo({ top: 0 });
+  }
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    if (this.menuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  toggleMenuRouter() {
+    if (this.menuOpen) {
+      this.menuOpen = false;
+      document.body.style.overflow = '';
+    }
+    this.scrollToTop();
+  }
+
+}
+
